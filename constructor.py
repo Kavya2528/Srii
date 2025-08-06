1766fccf925f55c531a086debf0460e9da9435be
Kavya2528 <vepurikavya8@gmail.com>
2025-08-06 17:14:22 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,107 @@
+#CONSTRUCTOR
+
+# # import time
+# class A_class:
+#     def __init__(self):
+#         print("This is constructor_method/function")
+# a1=A_class()
+# print()
+# time.sleep(2)
+# print("End of an application")
+# import time
+# class A_class:
+#     def __init__(x1):
+#         print("This is the constructor_method")
+# a1=A_class()
+# print()
+# time.sleep(2)
+# print("End of an application")
+# import time
+# class A_class:
+#     def __init__():
+#         print("This is the constructor_method")
+# a1=A_class()
+# print()
+# time.sleep(2)
+# print("End of an application")/
+# import time
+# class A_class:
+#     def __init__(self):
+#         print("This is the constructor_method")
+# a1=A_class()
+# print()
+# a2=A_class()
+# print()
+# a3=A_class()
+# print()
+# time.sleep(2)
+# print("End of an application")
+
+#INSTANCE_METHOD
+
+# import time
+# class B_class():
+#     def m1(self):
+#         print("This is instance_method/Non static method")
+# b1=B_class()
+# b1.m1()
+# print()
+# time.sleep(2)
+# print("End of an application")
+
+#CLASS METHOD
+# import time
+# class c_class:
+#     def m2(self):
+#         print("This is class method")
+# c1=c_class()
+# print()
+# c1.m2()
+# print()
+# time.sleep(2)
+# print("End of an application")
+
+#STATIC METHOD
+# import time
+# class s_class:
+#     @staticmethod
+#     def m3():
+#         print("This is static method")
+# S1=s_class()
+# print()
+# s_class.m3()
+# print()
+# time.sleep(2)
+# print("End of an application")
+
+# import time
+# class s_class:
+#     @staticmethod
+#     def m4(x1,x2):
+#         print("sum is::",x1+x2)
+# r1=s_class()
+# r1.m4(30,40)
+# print()
+# s_class.m4(30,40)
+# print()
+# time.sleep(2)
+# print("End of an application")
+
+# import time
+# class product_class:
+#     def __init__(self):
+#         self.pid=1001
+#         self.pname="Mobile_1"
+#         self.price=23000
+#         self.company="samsung"
+#     def m1(self):
+#         print("====product_details====")
+#         print("pid is:",self.pid)
+#         print("pname is:",self.pname)
+#         print("price is:",self.price)
+#         print("company is:",self.company)
+# p1=product_class()
+# p1.m1()
+# print()
+# time.sleep(2)
+# print("End of an application")
