598d863cbd2ef064343c6f2eb333d245f4745ace
Kavya2528 <vepurikavya8@gmail.com>
2025-08-06 17:12:25 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,43 @@
+#import time
+#food=input("Enter The fav_food:")
+#if(food=="Biriyani"):
+ #   print(food,":I eat the biriyani on every sunday")
+#print()
+#time.sleep(2)
+#print("end of the application:")
+
+
+#import time
+#Game=input("Enter The Game:")
+#if(Game=="chess"):
+ #   print(Game,":I  play this game  for mind refreshing")
+#print()
+#time.sleep(2)
+#print("end of the application:")
+
+#import time
+#place=input("Enter The place:")
+#if(place=="home"):
+ #   print(place,":I like home ,because of the spending the time with family it gives happiness to me")
+#print()
+#time.sleep(2)
+#print("end of the application:")
+
+#import time
+#color=input("Enter The color:")
+#if(color=="black"):
+ #   print(color,":Black is my fav color it attracts more to me")
+#print()
+#time.sleep(2)
+#print("end of the application:")
+
+#import time
+#cool_drink=input("Enter The cool_drink:")
+#if(cool_drink=="thumsup"):
+ #   print(cool_drink,":Thumsup is my fav cool_drink")
+#print()
+#time.sleep(2)
+#print("end of the application:")
+
+
+
